67caa633cd70c75f26e8cf83c3226002bd9c9338
Emile <emiletal@gmail.com>
2025-01-07 00:25:35 +0100

    Tweak padding for input forms and button

--- app/routes/create.tsx
+++ app/routes/create.tsx
@@ -33,20 +33,20 @@ const CreateEvent = () => {
                 <h1 className="text-4xl font-bold pb-4 text-gray-800 font-serif md:text-5xl md:pb-8">New Event</h1>
                 <div className="md:flex md:gap-8 md:items-start md:justify-between">
                     <Form method='post' className="md:flex-grow">
-                        <div className="flex flex-col gap-1 py-4">
+                        <div className="flex flex-col gap-1 py-4 md:gap-2">
                             <label htmlFor="title" className="text-gray-800 md:text-lg">Event Title: </label>
-                            <input type="text" name="title" id="title" className="py-2 px-2 rounded-xl outline-none text-sm md:text-lg" placeholder="Tim's Birthday Party" />
+                            <input type="text" name="title" id="title" className="p-2 rounded-xl outline-none text-sm md:text-lg md:p-4" placeholder="Tim's Birthday Party" />
                         </div>
-                        <div className="flex flex-col gap-1 py-4">
+                        <div className="flex flex-col gap-1 py-4 md:gap-2">
                             <label htmlFor="description" className="text-gray-800 md:text-lg">Event Description: </label>
-                            <textarea name="description" id="description" className="py-2 px-2 rounded-xl outline-none text-sm h-20 align-top resize-none md:text-lg" placeholder="Beer tasting at local microbrewery..."></textarea>
+                            <textarea name="description" id="description" className="p-2 rounded-xl outline-none text-sm h-20 align-top resize-none md:text-lg md:p-4 md:h-40" placeholder="Beer tasting at local microbrewery..."></textarea>
                         </div>
-                        <div className="flex flex-col gap-1 py-4">
+                        <div className="flex flex-col gap-1 py-4 md:gap-2">
                             <label htmlFor="date" className="text-gray-800 md:text-lg">Event Date: </label>
-                            <input type="date" name="date" id="date" className="py-2 px-2 rounded-xl outline-none text-sm md:text-lg" />
+                            <input type="date" name="date" id="date" className="p-2 rounded-xl outline-none text-sm md:text-lg md:p-4" />
                         </div>
                         <div className="flex items-center justify-center py-8">
-                            <button type="submit" className="text-gray-800 bg-gray-200 py-2 px-4 rounded-2xl hover:cursor-pointer hover:bg-gray-800 hover:text-gray-200 text-xl font-serif md:text-3xl">Create!</button>
+                            <button type="submit" className="text-gray-800 bg-gray-200 py-2 px-4 rounded-2xl hover:cursor-pointer hover:bg-gray-800 hover:text-gray-200 text-xl font-serif md:text-3xl md:py-4 md:px-8">Create!</button>
                         </div>
                     </Form>
                     <div className="w-full flex justify-center md:w-2/5">
