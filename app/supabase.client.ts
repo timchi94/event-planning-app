48d0c446c8948886dab91bbde1a2d885df5dc4e9
Tim Chiu <timothychiu1994@gmail.com>
2025-01-08 11:51:26 -0500

    better logging for supabase client

--- app/supabase.client.ts
+++ app/supabase.client.ts
@@ -3,7 +3,8 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.SUPABASE_URL || ""; 
 const supabaseKey = process.env.SUPABASE_ANON_KEY || ""; 
 
-console.log(supabaseUrl, supabaseKey)
+console.log("Supabase URL:", process.env.SUPABASE_URL);
+console.log("Supabase Key:", process.env.SUPABASE_ANON_KEY);
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("supabaseUrl and supabaseKey must be set in environment variables");
