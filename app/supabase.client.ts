d8fb9d1c5f6946252ada297d689d600cba0cd659
Tim Chiu <timothychiu1994@gmail.com>
2025-01-08 11:47:14 -0500

    logged client

--- app/supabase.client.ts
+++ app/supabase.client.ts
@@ -3,6 +3,8 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.SUPABASE_URL || ""; 
 const supabaseKey = process.env.SUPABASE_ANON_KEY || ""; 
 
+console.log(supabaseUrl, supabaseKey)
+
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("supabaseUrl and supabaseKey must be set in environment variables");
 }
